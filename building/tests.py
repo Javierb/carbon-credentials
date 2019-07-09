2c95eb3b41c79b35af876e3f6eecc8de57eeb239
Javier <fjbordalas@gmail.com>
2019-07-09 13:20:12 +0200

    Add failing test for importing data

--- building/tests.py
+++ building/tests.py
@@ -3,6 +3,10 @@ from django.utils import timezone
 from django.db.utils import IntegrityError
 from django.utils import timezone, dateparse
 from .models import Building, Meter, Energy
+from import_export import resources
+from tablib import Dataset
+import os
+
 
 
 class BaseBuildingTestCase(TestCase):
@@ -14,8 +18,29 @@ class BaseBuildingTestCase(TestCase):
 
 
 class BuildingTestCase(BaseBuildingTestCase):
-    def test_halfhourly_integrity(self):
-        other_halfhour = Energy(meter=self.meter, consumption=44.835, reading_date_time=self.dt)
-        with self.assertRaises(IntegrityError):
-            other_halfhour.save()
+
+    # def test_halfhourly_integrity(self):
+    #     other_halfhour = Energy(meter=self.meter, consumption=44.835, reading_date_time=self.dt)
+    #     with self.assertRaises(IntegrityError):
+    #         other_halfhour.save()
+
+    def test_csv_import(self):
+        csv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'csv/building_data.csv')
+        building_resource = resources.modelresource_factory(model=Building)()
+        building_dataset = Dataset().load(open(csv_path).read())
+        result = building_resource.import_data(building_dataset)
+        self.assertEqual(result.has_errors(), False)
+
+        csv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'csv/meter_data.csv')
+        meter_resource = resources.modelresource_factory(model=Meter)()
+        meter_dataset = Dataset().load(open(csv_path).read())
+        result = meter_resource.import_data(meter_dataset, dry_run=False)
+        self.assertEqual(result.has_errors(), False)
+
+        csv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'csv/halfhourly_data.csv')
+        energy_resource = resources.modelresource_factory(model=Energy)()
+        energy_dataset = Dataset().load(open(csv_path).read())
+        result = energy_resource.import_data(energy_dataset, dry_run=False)
+        self.assertEqual(result.has_errors(), False)
+    
 
