2befb352c05f3b60036c3001bc6e56d93bc830e2
Javier <fjbordalas@gmail.com>
2019-07-12 03:50:42 +0200

    Use SQLite for testing

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -1,10 +1,18 @@
+import sys
 import os
 
 DEV_MODE = 'development'
 PROD_MODE = 'production'
+
 mode = os.environ.get('DJANGO_ENVIRONMENT_MODE', DEV_MODE)
 
 if mode == PROD_MODE:
     from .production import *
 else:
     from .development import *
+
+
+if 'test' in sys.argv or 'test_coverage' in sys.argv:
+    DATABASES = {
+        'default': env.db('SQLITE_URL', default='sqlite://../testing.db')
+    }
